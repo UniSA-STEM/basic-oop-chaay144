3dce8c96e138f9a14398f00fd44aa4139a1dfb98
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-10 13:08:43 +1030

    Made class asset in Asset.py file

--- Asset.py
+++ Asset.py
@@ -6,3 +6,9 @@ ID: 110369235
 Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.encrypted = False
