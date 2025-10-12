ca399916c619ce3cf757a9ba86a9e43ed4f83dac
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-12 18:22:02 +1030

    Updating encryptable.py file

--- encryptable.py
+++ encryptable.py
@@ -6,3 +6,24 @@ ID: 110369235
 Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+from abc import ABC, abstractmethod
+
+class Encryptable(ABC):
+    """
+    Very small abstract base that says an object must be encryptable.
+    This keeps the design clear but stays simple for the assignment.
+    """
+
+    @property
+    @abstractmethod
+    def encrypted(self) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def encrypt(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def decrypt(self) -> None:
+        raise NotImplementedError
