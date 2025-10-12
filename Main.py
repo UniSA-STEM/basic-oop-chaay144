406c362fe5b0fe0a1ad28a696e842be14a64776b
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-12 18:04:36 +1030

    just checking the security_chip.py file

--- Main.py
+++ Main.py
@@ -7,5 +7,12 @@ Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
+from security_chip import SecurityChip
+
+c = SecurityChip(uses=2)
+print(c, c.has_charge())
+print("use1:", c.use(), "remaining:", c.uses)
+print("use2:", c.use(), "remaining:", c.uses)
+print("use3:", c.use(), "remaining:", c.uses)
 
 
