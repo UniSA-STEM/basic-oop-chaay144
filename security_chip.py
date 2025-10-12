8a7cf6d99f07256480f7edb6a6979a618b20e89c
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-12 17:54:24 +1030

    create a security_chip.py file

--- security_chip.py
+++ security_chip.py
@@ -2,7 +2,7 @@
 File: security.py
 Description: <A brief description of this Python module.>
 Description: <A brief description of this Python module.>
-Author: ansh Channa
+Author: Ansh Channa
 ID: 110369235
 Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
