c37ab4fa0e078c63096c7a4b8d45a50765d92d4b
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-12 18:20:47 +1030

    Create encryptable.py file

---
+++ encryptable.py
@@ -0,0 +1,8 @@
+"""
+File: encryptable.py
+Description: <A brief description of this Python module.>
+Author: Ansh Channa
+ID: 110369235
+Username: chaay144
+This is my own work as defined by the University's Academic Misconduct Policy.
+"""
