062e6275613f63308cf2118788f1f1bb5686d48b
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-12 16:44:19 +1030

    Updating the header in Hacker.py, Main.py, Rig.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 """
 File: main.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Ansh Channa
+ID: 110369235
+Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
