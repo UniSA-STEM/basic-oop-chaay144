39e7c8029bc5ebe475e49ca8d2e4afe1faeb8740
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-12 17:10:13 +1030

    just  checking the asset.py file is working or not

--- Main.py
+++ Main.py
@@ -6,3 +6,12 @@ ID: 110369235
 Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+if __name__ == "__main__":
+    from asset import Asset
+    a = Asset("Data Chip", "Sector 7 intel")
+    a.display()
+    a.encrypt()
+    a.display()
+    a.decrypt()
+    a.display()
