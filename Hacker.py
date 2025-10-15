004e0ec9ab50d3e24e05b3f17f5219017237fed5
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-16 02:15:19 +1030

    defining getters and setters in Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -18,3 +18,26 @@ class Hacker:
         self.__trace_level = 0
         self.__inventory = [Asset("CryptoToken", "Used to acquire or repair rigs.")]
         self.__rig = None
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, new_name):
+        if isinstance(new_name, str) and new_name.strip():
+            self.__name = new_name
+
+    def get_trace_level(self):
+        return self.__trace_level
+
+    def set_trace_level(self, value):
+        if isinstance(value, int) and value >= 0:
+            self.__trace_level = value
+
+    def get_inventory(self):
+        return self.__inventory
+
+    def get_rig(self):
+        return self.__rig
+
+    def set_rig(self, rig_obj):
+        self.__rig = rig_obj
