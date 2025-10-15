89ae928cf5c1011f85f0fb3ce6a810e861db70c2
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-15 12:22:04 +1030

    Checking the Asset class

--- Main.py
+++ Main.py
@@ -7,7 +7,17 @@ Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
+#Checking the Asset class
+from asset import Asset
 
+token = Asset("CryptoToken", "Used to acquire or repair rigs")
+chip = Asset("Security Chip", "Used for encryption and decryption")
+
+print(token)
+chip.encrypt()
+print(chip)
+chip.decrypt()
+print(chip)
 
 
 
