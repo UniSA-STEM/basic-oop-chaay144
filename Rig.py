12a044683ff01aca5d704c356d43fac074d61113
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-15 12:29:06 +1030

    Define condition string based on damage and upgrade level

--- Rig.py
+++ Rig.py
@@ -9,22 +9,24 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 from asset import Asset
 class Rig:
-    def __init__(self, name: str):
-        self.name: str = name
-        self.damage: int = 0
-        self.broken: bool = False
-        self.upgrade_level: int = 0
+    def __init__(self, name):
+        self.name = name
+        self.damage = 0
+        self.broken = False
+        self.upgrade_level = 0
         self.storage=[Asset("Data spike","used in"),
                       Asset("Data Spike", "Used in attacks"),
                       Asset("Removable Drive", "Used to extract assets")
                       ]
 
     def condition(self):
+        """Return condition string based on damage and upgrade level."""
         if self.broken:
             return f"Broken (Level {self.upgrade_level})"
-        if self.damage == 0:
+        elif self.damage == 0:
             return f"Pristine (Level {self.upgrade_level})"
-        return f"Damaged (Level {self.upgrade_level})"
+        else:
+            return f"Damaged (Level {self.upgrade_level})"
 
     def __str__(self):
         if not self.storage:
