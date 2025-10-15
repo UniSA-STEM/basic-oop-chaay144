ded2f1071b8e98fa19155d57ee7d47a50212ba5a
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-15 13:39:38 +1030

    tested the Rig file

--- Main.py
+++ Main.py
@@ -7,9 +7,11 @@ Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
-#Checking the Asset class
-from asset import Asset
 
+from asset import Asset
+from Rig import Rig
+
+#Checking the Asset class
 token = Asset("CryptoToken", "Used to acquire or repair rigs")
 chip = Asset("Security Chip", "Used for encryption and decryption")
 
@@ -19,6 +21,19 @@ print(chip)
 chip.decrypt()
 print(chip)
 
+
+
+#Checking the Rig class
+r = Rig("CyberDeck")
+token = Asset("CryptoToken", "Used to buy rigs")
+chip = Asset("Security Chip", "For encryption")
+chip.encrypt()
+
+r.store_asset(token)   # should store
+r.store_asset(chip)    # should block (encrypted)
+r.release_asset("CryptoToken")  # should release
+r.release_asset("Security Chip")  # should block
+print(r)
 
 
 
