7622a52d320cacd3482106f995911c56a692a56f
chaay144 <133758561+chaay144@users.noreply.github.com>
2025-10-16 02:14:18 +1030

    defining initialiser in Hacker class

--- Hacker.py
+++ Hacker.py
@@ -6,13 +6,15 @@ ID: 110369235
 Username: chaay144
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
-from asset import Asset
 
+from asset import Asset
+from Rig import Rig
+
+TRACE_LIMIT = 5
 
 class Hacker:
     def __init__(self, name):
-        self.name = name
-        self.trace_level = 0
-        self.inventory = [Asset("Crypto_token", "Used to acquire  or repair rigs")]
-        self.rig = None
-
+        self.__name = name
+        self.__trace_level = 0
+        self.__inventory = [Asset("CryptoToken", "Used to acquire or repair rigs.")]
+        self.__rig = None
